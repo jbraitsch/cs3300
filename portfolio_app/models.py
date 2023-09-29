ab69f3daf708750c1536ec3abd567014e23286ee
jbraitsch <joe.braitsch3@gmail.com>
2023-09-28 19:51:10 -0600

    Add functions to Student.

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Student(models.Model):
 
@@ -12,7 +13,17 @@ class Student(models.Model):
     ('CSCI-BA', 'BA in Computer Science'),
     ('DASE-BS', 'BS in Data Analytics and Systems Engineering')
     )
-name = models.CharField(max_length=200)
-email = models.CharField("UCCS Email", max_length=200)
-major = models.CharField(max_length=200, choices=MAJOR, blank = True)
+    name = models.CharField(max_length=200)
+    email = models.CharField("UCCS Email", max_length=200)
+    major = models.CharField(max_length=200, choices = MAJOR)
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('student-detail', args=[str(self.id)])
 
