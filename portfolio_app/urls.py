186e844eb77a777a803427e7cda0e56d00a194cd
jbraitsch <joe.braitsch3@gmail.com>
2023-10-16 13:24:15 -0600

    project and portfolio pages working

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('students/', views.StudentListView.as_view(), name= 'students'),
     path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
     path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail'),
+    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail' ),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from .models import Student, Portfolio
+from .models import Student, Portfolio, Project
 
 # Create your views here.
 def index(request):
     student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
-   # student_active_portfolios = student_active_portfolios.filter(is)
     print("active portfolio query set", student_active_portfolios)
     return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
 
@@ -18,3 +17,12 @@ class StudentDetailView(generic.DetailView):
     model = Student
 class PortfolioDetailView(generic.DetailView):
     model = Portfolio
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the p
+        context["project_list"] = self.object.project_set.all()
+        return context
+class ProjectDetailView(generic.DetailView):
+    model = Project
